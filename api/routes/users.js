62fd5a6f2442d1a2eed590ede424f2f1a2de0b73
HotDogCoder <jorgehospinalflores@gmail.com>
2021-04-06 06:27:25 -0500

    hola 2

--- api/routes/users.js
+++ api/routes/users.js
@@ -3,6 +3,21 @@ const router = express.Router();
 const User = require('../models/user');
 const mongoose = require('mongoose');
 
+router.get('/', (req, res, next) => {
+    User.find()
+    .exec()
+    .then(result => {
+        res.status(200).json(result);
+    })
+    .catch(err => {
+        res.status(500).json({
+            error: err,
+            msg: 'error al traer usuarios'
+        });
+    })
+});
+
+
 router.post('/', (req, res, next) => {
     /*new user = {
         name: req.body.name,
